d1ea968d915e00fa397a20310e87ed8f09e8ae8f
Ulf Eliasson <ulf.l.eliasson@gmail.com>
2019-12-01 08:25:36 +0100

    Add solution for Advent of Code 2019 day 01 part 2

--- 2019/01/aoc_01.py
+++ 2019/01/aoc_01.py
@@ -32,19 +32,71 @@ input), then add together all the fuel values.
 
 What is the sum of the fuel requirements for all of the modules on your
 spacecraft?
+
+--- Part Two ---
+
+During the second Go / No Go poll, the Elf in charge of the Rocket Equation
+Double-Checker stops the launch sequence. Apparently, you forgot to include
+additional fuel for the fuel you just added.
+
+Fuel itself requires fuel just like a module - take its mass, divide by three,
+round down, and subtract 2. However, that fuel also requires fuel, and that
+fuel requires fuel, and so on. Any mass that would require negative fuel should
+instead be treated as if it requires zero fuel; the remaining mass, if any, is
+instead handled by wishing really hard, which has no mass and is outside the
+scope of this calculation.
+
+So, for each module mass, calculate its fuel and add it to the total. Then,
+treat the fuel amount you just calculated as the input mass and repeat the
+process, continuing until a fuel requirement is zero or negative. For example:
+
+    A module of mass 14 requires 2 fuel. This fuel requires no further fuel (2
+        divided by 3 and rounded down is 0, which would call for a negative
+        fuel), so the total fuel required is still just 2.
+    At first, a module of mass 1969 requires 654 fuel. Then, this fuel requires
+        216 more fuel (654 / 3 - 2). 216 then requires 70 more fuel, which
+        requires 21 fuel, which requires 5 fuel, which requires no further
+        fuel. So, the total fuel required for a module of mass 1969 is
+        654 + 216 + 70 + 21 + 5 = 966.
+    The fuel required by a module of mass 100756 and its fuel is:
+        33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346.
+
+What is the sum of the fuel requirements for all of the modules on your
+spacecraft when also taking into account the mass of the added fuel? (Calculate
+the fuel requirements for each module separately, then add them all up at the
+end.)
 """
 import math
 
 import aoc_01_input
 
+
 def calculate_fuel(mass):
     return math.floor(mass / 3) - 2
 
-def calculate_total_fuel(masses):
-    return sum(map(calculate_fuel, masses))
+
+def calculate_total_fuel(masses, fuel_func=calculate_fuel):
+    return sum(map(fuel_func, masses))
+
+
+def calculate_total_module_fuel(mass):
+    total_fuel = 0
+
+    while (fuel := calculate_fuel(mass)) > 0:
+        total_fuel += fuel
+        mass = fuel
+
+    return total_fuel
+
+
+def calculate_correct_total_fuel(masses):
+    return calculate_total_fuel(masses, fuel_func=calculate_total_module_fuel)
+
 
 def main():
     print(f"part 01: {calculate_total_fuel(aoc_01_input.get_input())}")
+    print(f"part 02: {calculate_correct_total_fuel(aoc_01_input.get_input())}")
+
 
 if __name__ == "__main__":
     main()
--- 2019/01/test_aoc_01.py
+++ 2019/01/test_aoc_01.py
@@ -2,13 +2,9 @@ import pytest
 
 import aoc_01
 
+
 @pytest.mark.parametrize(
-    "mass, expected_fuel", [
-        (12, 2),
-        (14, 2),
-        (1969, 654),
-        (100756, 33583),
-    ]
+    "mass, expected_fuel", [(12, 2), (14, 2), (1969, 654), (100756, 33583),]
 )
 def test_calculate_fuel(mass, expected_fuel):
     """
@@ -19,5 +15,29 @@ def test_calculate_fuel(mass, expected_fuel):
     """
     assert aoc_01.calculate_fuel(mass) == expected_fuel
 
+
 def test_calculate_total_fuel():
     assert aoc_01.calculate_total_fuel([12, 14, 1969, 100756]) == 2 + 2 + 654 + 33583
+
+
+@pytest.mark.parametrize(
+    "mass, expected_fuel", [(14, 2), (1969, 966), (100756, 50346),]
+)
+def test_calculate_total_module_fuel(mass, expected_fuel):
+    """
+    A module of mass 14 requires 2 fuel. This fuel requires no further fuel (2
+        divided by 3 and rounded down is 0, which would call for a negative
+        fuel), so the total fuel required is still just 2.
+    At first, a module of mass 1969 requires 654 fuel. Then, this fuel requires
+        216 more fuel (654 / 3 - 2). 216 then requires 70 more fuel, which
+        requires 21 fuel, which requires 5 fuel, which requires no further
+        fuel. So, the total fuel required for a module of mass 1969 is
+        654 + 216 + 70 + 21 + 5 = 966.
+    The fuel required by a module of mass 100756 and its fuel is:
+        33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346.
+    """
+    assert aoc_01.calculate_total_module_fuel(mass) == expected_fuel
+
+
+def test_calculate_correct_total_fuel():
+    assert aoc_01.calculate_correct_total_fuel([14, 1969, 100756]) == 2 + 966 + 50346
