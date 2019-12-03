1558b1bc32286d89863a8203c65760c30f4f877a
Ulf Eliasson <ulf.l.eliasson@gmail.com>
2019-12-03 18:49:56 +0100

    Add solution for Advent of Code 2019 day 02 part 2

--- 2019/02/aoc_02.py
+++ 2019/02/aoc_02.py
@@ -83,16 +83,66 @@ assist program (your puzzle input) to the "1202 program alarm" state it had
 just before the last computer caught fire. To do this, before running the
 program, replace position 1 with the value 12 and replace position 2 with the
 value 2. What value is left at position 0 after the program halts?
+
+
+--- Part Two ---
+
+"Good, the new computer seems to be working correctly! Keep it nearby during
+this mission - you'll probably use it again. Real Intcode computers support
+many more features than your new one, but we'll let you know what they are as
+you need them."
+
+"However, your current priority should be to complete your gravity assist
+around the Moon. For this mission to succeed, we should settle on some
+terminology for the parts you've already built."
+
+Intcode programs are given as a list of integers; these values are used as the
+initial state for the computer's memory. When you run an Intcode program, make
+sure to start by initializing memory to the program's values. A position in
+memory is called an address (for example, the first value in memory is at
+"address 0").
+
+Opcodes (like 1, 2, or 99) mark the beginning of an instruction. The values
+used immediately after an opcode, if any, are called the instruction's
+parameters. For example, in the instruction 1,2,3,4, 1 is the opcode; 2, 3, and
+4 are the parameters. The instruction 99 contains only an opcode and has no
+parameters.
+
+The address of the current instruction is called the instruction pointer; it
+starts at 0. After an instruction finishes, the instruction pointer increases
+by the number of values in the instruction; until you add more instructions to
+the computer, this is always 4 (1 opcode + 3 parameters) for the add and
+multiply instructions. (The halt instruction would increase the instruction
+pointer by 1, but it halts the program instead.)
+
+"With terminology out of the way, we're ready to proceed. To complete the
+gravity assist, you need to determine what pair of inputs produces the output
+19690720."
+
+The inputs should still be provided to the program by replacing the values at
+addresses 1 and 2, just like before. In this program, the value placed in
+address 1 is called the noun, and the value placed in address 2 is called the
+verb. Each of the two input values will be between 0 and 99, inclusive.
+
+Once the program has halted, its output is available at address 0, also just
+like before. Each time you try a pair of inputs, make sure you first reset the
+computer's memory to the values in the program (your puzzle input) - in other
+words, don't reuse memory from a previous attempt.
+
+Find the input noun and verb that cause the program to produce the output
+19690720. What is 100 * noun + verb? (For example, if noun=12 and verb=2, the
+answer would be 1202.)
 """
 
+import copy
 import operator
 
 import aoc_02_input
 
 
-def get_operator(op_code):
+def get_operator(opcode):
     operators = {1: operator.add, 2: operator.mul}
-    return operators[op_code]
+    return operators[opcode]
 
 
 def restore_program(program):
@@ -102,24 +152,39 @@ def restore_program(program):
 
 
 def run_program(program):
-    current_pos = 0
+    instruction_pointer = 0
 
-    while (op_code := program[current_pos]) != 99:
-        operator = get_operator(op_code)
+    while (opcode := program[instruction_pointer]) != 99:
+        operator = get_operator(opcode)
 
-        source_1 = program[current_pos + 1]
-        source_2 = program[current_pos + 2]
-        destination = program[current_pos + 3]
+        parameter_1 = program[instruction_pointer + 1]
+        parameter_2 = program[instruction_pointer + 2]
+        destination = program[instruction_pointer + 3]
 
-        program[destination] = operator(program[source_1], program[source_2])
-        current_pos += 4
+        program[destination] = operator(program[parameter_1], program[parameter_2])
+        instruction_pointer += 4
 
     return program
+
+
+def find_noun_and_verb(memory, value_to_find):
+    for noun in range(0, 100):
+        for verb in range(0, 100):
+            next_memory = copy.deepcopy(memory)
+            next_memory[1] = noun
+            next_memory[2] = verb
+            if run_program(next_memory)[0] == value_to_find:
+                return (noun, verb)
+    return (None, None)
 
 
 def main():
     instructions = restore_program(aoc_02_input.get_instructions())
     print(f"part_1: {run_program(instructions)[0]}")
+
+    instructions = aoc_02_input.get_instructions()
+    (noun, verb) = find_noun_and_verb(instructions, 19690720)
+    print(f"part_2: {noun*100+verb}")
 
 
 if __name__ == "__main__":
