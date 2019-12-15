f6f2a6011d9d060e617a698f29d349db5f052911
Ulf Eliasson <ulf.l.eliasson@gmail.com>
2019-12-15 22:17:43 +0100

    Add solution for Advent of Code 2019 day 04 part 2

--- 2019/04/aoc_04.py
+++ 2019/04/aoc_04.py
@@ -22,6 +22,24 @@ Other than the range rule, the following are true:
 How many different passwords within the range given in your puzzle input meet
 these criteria?
 
+--- Part Two ---
+
+An Elf just remembered one more important detail: the two adjacent matching
+digits are not part of a larger group of matching digits.
+
+Given this additional criterion, but still ignoring the range rule, the
+following are now true:
+
+    112233 meets these criteria because the digits never decrease and all
+        repeated digits are exactly two digits long.
+    123444 no longer meets the criteria (the repeated 44 is part of a larger
+        group of 444).
+    111122 meets the criteria (even though 1 is repeated more than twice, it
+        still contains a double 22).
+
+How many different passwords within the range given in your puzzle input meet
+all of the criteria?
+
 Your puzzle input is 183564-657474.
 """
 
@@ -50,11 +68,47 @@ def is_valid_password(password: int) -> bool:
     return found_pair
 
 
+def is_valid_password_part_2(password: int) -> bool:
+    password_string = str(password)
+
+    previous_digit = int(password_string[0])
+
+    potential_pair = False
+    larger_group = False
+    found_pair = False
+
+    for digit in password_string[1:]:
+        digit = int(digit)
+
+        if digit == previous_digit and not (potential_pair or larger_group):
+            potential_pair = True
+        elif potential_pair and digit == previous_digit:
+            potential_pair = False
+            larger_group = True
+        elif potential_pair and digit != previous_digit:
+            found_pair = True
+            potential_pair = False
+        elif larger_group and digit != previous_digit:
+            larger_group = False
+
+        if digit < previous_digit:
+            return False
+
+        previous_digit = digit
+
+    return found_pair or potential_pair
+
+
 def main():
-    number_of_valid_in_range = len(
-        [password for password in range(start, stop + 1) if is_valid_password(password)]
-    )
-    print(f"part 1: {number_of_valid_in_range}")
+    valid_part_1 = [
+        password for password in range(start, stop + 1) if is_valid_password(password)
+    ]
+    valid_part_2 = [
+        password for password in valid_part_1 if is_valid_password_part_2(password)
+    ]
+
+    print(f"part 1: {len(valid_part_1)}")
+    print(f"part 2: {len(valid_part_2)}")
 
 
 if __name__ == "__main__":
--- 2019/04/test_aoc_04.py
+++ 2019/04/test_aoc_04.py
@@ -6,10 +6,35 @@ import aoc_04
 @pytest.mark.parametrize(
     "password, valid", [(111111, True), (223450, False), (123789, False)]
 )
-def test_is_valid_password(password, valid):
+def test_is_valid_password_part_1(password, valid):
     """
     111111 meets these criteria (double 11, never decreases).
     223450 does not meet these criteria (decreasing pair of digits 50).
     123789 does not meet these criteria (no double).
     """
     assert aoc_04.is_valid_password(password) == valid
+
+
+@pytest.mark.parametrize(
+    "password, valid",
+    [
+        (112233, True),
+        (123444, False),
+        (111122, True),
+        (122112, False),
+        (122345, True),
+        (112333, True),
+        (111244, True),
+        (168888, False),
+    ],
+)
+def test_is_valid_password_part_2(password, valid):
+    """
+    112233 meets these criteria because the digits never decrease and all
+        repeated digits are exactly two digits long.
+    123444 no longer meets the criteria (the repeated 44 is part of a larger
+        group of 444).
+    111122 meets the criteria (even though 1 is repeated more than twice, it
+        still contains a double 22).
+    """
+    assert aoc_04.is_valid_password_part_2(password) == valid
